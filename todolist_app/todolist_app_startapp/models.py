f8e3f76baae0a6b7625adef28b553c6e6363ec2c
crazymuchacho <GithubG7158>
2023-02-24 13:31:47 +0500

    first commit

---
+++ todolist_app/todolist_app_startapp/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.conf import settings
+from django_extensions.db.models import TimeStampedModel
+import datetime
+
+
+# Create your models here.
+
+
+class table_Task(models.Model):
+    """Задачи в todolist"""
+
+    task = models.CharField(max_length=1024, help_text="Формулировка задачи")
+    task_is_Done = models.BooleanField(default=False, help_text="Задача выполнена")
+    task_create = models.DateTimeField(
+        auto_now_add=True, help_text="Дата/время создания задачи"
+    )
+    task_completed = models.DateTimeField(
+        blank=True, null=True, help_text="Дата/время завершения задачи"
+    )
+
+    def save(self, *args, **kwargs) -> None:
+        """Переопределение сохранения.
+
+        Убирается галочка активности корзины если нет привязанного пользователя.
+        """
+        if self.task_is_Done is False:
+            self.task_completed = None
+        else:
+            time_now=datetime.datetime.utcnow()
+            self.task_completed=time_now
+        return super().save(*args, **kwargs)
+    
+    def __str__(self):
+        """переопределение строкового представления объекта."""
+        return f"Task {self.id}|{self.task}|{self.task_is_Done}|{self.task_create}|{self.task_completed}"
---
+++ todolist_app/todolist_app_startapp/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from todolist_app_startapp.models import table_Task
+from django.views.generic import (
+    DetailView,
+    ListView,
+    CreateView,
+    UpdateView,
+    View,
+    DeleteView
+)
+
+# Create your views here.
+class TaskListView(ListView):
+    """Представление для отображения задач.
+
+    .._ https://docs.djangoproject.com/en/4.1/ref/class-based-views/generic-display/#listview
+    """
+
+    context_object_name = "tasks_"
+    queryset = table_Task.objects.all()
+    # model = Cart
+    template_name = "task_list.html"
+
+class TaskCreateView(CreateView):
+    """Представление для создания одной корзины.
+    
+    .._ https://docs.djangoproject.com/en/4.1/ref/class-based-views/generic-editing/#django.views.generic.edit.CreateView
+    """
+    model = table_Task
+    fields = ['task', 'task_is_Done']
+    template_name = "task_create.html"
+    success_url = '/tasks/'
+
+class TaskDeleteView(DeleteView):
+    context_object_name = "task_del"
+    model=table_Task
+    fields = ['task', 'task_is_Done']
+    template_name = "task_delete.html"
+    success_url = '/tasks/'
+
+
+class TaskUpdateView(UpdateView):
+    """Представление для создания одной корзины.
+    
+    .._ https://docs.djangoproject.com/en/4.1/ref/class-based-views/generic-editing/#django.views.generic.edit.CreateView
+    """
+    model = table_Task
+    fields = ['task', 'task_is_Done']
+    template_name = "task_edit.html"
+    success_url = '/tasks/'
+
